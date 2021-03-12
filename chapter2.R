11cd61179b67030441b5dd5b5825debd5a620948
공순재 <hanwhalife@gongsunjaeui-MacBookPro.local>
2021-03-12 11:04:30 +0900

    수정

--- chapter2.R
+++ chapter2.R
@@ -78,6 +78,84 @@ matrix(hand2, nrow = 2, byrow = TRUE)
 matrix(hand2, ncol = 2, byrow = TRUE)
 
 
+die <- c(1, 2, 3, 4, 5, 6)
+typeof(die)     #"double"
+class(die)      #"numeric"
+attributes(die) #NULL 
+
+
+dim(die) <-c(2, 3)
+typeof(die) #"double"
+class(die)  #"matrix"
+attributes(die) 
+# $dim
+# [1] 2 3
 
 
 
+now <- Sys.time()
+now
+typeof(now) #"double"
+class(now)  #"POSIXct" "POSIXt"
+attributes(now) 
+# $class
+# [1] "POSIXct" "POSIXt"
+unclass(now) # 1615511086, 1970-01-01이후 시간을 초 단위로 계산 
+
+
+gender <- factor(c("male", "female", "female", "male"))
+typeof(gender)     #"integer"
+class(gender)      #"factor"
+attributes(gender) 
+# $levels
+# [1] "female" "male"  
+# $class
+# [1] "factor"
+unclass(gender)
+# [1] 2 1 1 2
+# attr(,"levels")
+# [1] "female" "male"
+
+
+
+list1 <- list(100:200, "R", list(TRUE, FALSE))
+list1
+
+card <- list("ace", "hearts", 1)
+card
+
+
+
+df <- data.frame(
+  face = c("ace", "two", "six"),
+  suit = c("clubs", "clubs", "clubs"),
+  value = c(1, 2, 3)
+)
+df
+typeof(df)     #"list"
+class(df)      #"data.frame"
+attributes(df) 
+# $names
+# [1] "face"  "suit"  "value"
+# $class
+# [1] "data.frame"
+# $row.names
+# [1] 1 2 3
+str(df)
+# 'data.frame':	3 obs. of  3 variables:
+# $ face : Factor w/ 3 levels "ace","six","two": 1 3 2
+# $ suit : Factor w/ 1 level "clubs": 1 1 1
+# $ value: num  1 2 3
+
+
+df <- data.frame(
+  face = c("ace", "two", "six"),
+  suit = c("clubs", "clubs", "clubs"),
+  value = c(1, 2, 3),
+  stringsAsFactors = FALSE
+)
+str(df)
+# 'data.frame':	3 obs. of  3 variables:
+# $ face : chr  "ace" "two" "six"
+# $ suit : chr  "clubs" "clubs" "clubs"
+# $ value: num  1 2 3
